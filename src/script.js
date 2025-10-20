bf3703e9dbfab338e4a28a55324d2f85ecfeb5a5
mahabur17 <m.rahman15424@gmail.com>
2025-10-20 18:44:36 +0600

    input logics build

--- src/script.js
+++ src/script.js
@@ -2,16 +2,23 @@ let addbtn=document.querySelector('#addbtn')
 let maininput=document.querySelector('#maininput')
 let inputdiv=document.querySelector('.inputs')
 let work=document.querySelector('.kire')
+const value=[]
+
 
 addbtn.addEventListener('click',()=>{
  
   if(maininput.value==""){
     alert('Please insert a value')
   }
-  else{
-work.innerHTML=maininput.value
- inputdiv.classList.remove('hidden');
-
+  if(maininput.value!=="" && value.includes(maininput.value) ){
+    alert('Already Exist')
   }
+   if(maininput.value!==""){
+work.innerHTML=maininput.value
+value.push(maininput.value)
+maininput.value=""
+inputdiv.classList.remove('hidden');
+  }
+  
   
 })
