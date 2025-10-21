98ba36cd9d26258dabb37a84e66932985fb19134
mahabur17 <m.rahman15424@gmail.com>
2025-10-22 01:06:26 +0600

    button function

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 let addbtn = document.querySelector('#addbtn');
 let maininput = document.querySelector('#maininput');
-let divcontainer = document.querySelector('.inputs'); // parent div
+let divcontainer = document.querySelector('.divcontainer'); // parent div
 let inputdiv = document.querySelector('.input'); // child div
 divcontainer.removeChild(inputdiv )
 const value = [];
@@ -18,20 +18,66 @@ addbtn.addEventListener('click', () => {
     return;
   }
 if(maininput.value!==""){
-  // clone the child div
   const newDiv = inputdiv.cloneNode(true);
   newDiv.style.marginBottom="8px";
-  
-  // change the text inside the cloned div
-  newDiv.querySelector('.kire').innerText = maininput.value;
 
-  // add the new value to the array
-  value.push(maininput.value);
+  newDiv.querySelector('.inputText').innerText = maininput.value;
+  value.push(maininput.value); 
 
-  // clear input
   maininput.value = "";
+// dlt button
+  let del=newDiv.querySelector('#delbtn')
+del.addEventListener('click',()=>{
+ divcontainer.removeChild(newDiv )
+})
+// tik button
+let p= newDiv.querySelector('.inputText').innerText;
+  let clear=newDiv.querySelector('#clear')
 
-  // ✅ append the cloned div into the parent container
+clear.addEventListener('click',()=>{
+  
+let inputText = newDiv.querySelector('.inputText');
+
+
+  if (clear.innerHTML.includes('fa-rotate-right')) {
+    
+    inputText.innerHTML = p;
+    clear.innerHTML = `<i class="fa-solid fa-check"></i>`;
+  } else {
+    
+    inputText.innerHTML = `<del>${p}</del>`;
+    clear.innerHTML = `<i class="fa-solid fa-rotate-right"></i>`;
+  }
+})
+
+// edit button
+let editbtn=newDiv.querySelector('#edit');
+editbtn.addEventListener('click',()=>{
+  let inputText = newDiv.querySelector('.inputText');
+
+  if(editbtn.innerHTML.includes('fas fa-pencil-alt')){
+    inputText.innerHTML=`<input type="text" id="editInput" value="${p}" class="editInput  px-2 rounded">`;
+    
+    let editinput = newDiv.querySelector('#editInput');
+    editinput.focus();
+    editinput.style.border='none'
+
+  editbtn.innerHTML='<i class="fa-solid fa-floppy-disk"></i>'
+  }
+
+ else if (editbtn.innerHTML.includes('fa-floppy-disk')) {
+   
+    let editinput = newDiv.querySelector('#editInput');
+    inputText.innerText = editinput.value;
+
+    
+    editbtn.innerHTML = '<i class="fas fa-pencil-alt"></i>';
+  }
+})
+
   divcontainer.appendChild(newDiv);
   }
+  
+
 });
+
